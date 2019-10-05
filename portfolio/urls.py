dc1880ff577423858e5ea83a37d51b10bb966a84
Stancw <yolo_stance@protonmail.com>
2019-10-05 22:02:37 +0200

    some fix

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -23,3 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]
+
